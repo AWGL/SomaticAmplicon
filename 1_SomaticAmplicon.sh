47b1b47c54016aabdd38784dc8b09ce051522e37
Christopher Medway <christopher.medway@wales.nhs.uk>
2018-11-15 10:30:48 +0000

    updated version variable to 1.7.6

--- 1_SomaticAmplicon.sh
+++ 1_SomaticAmplicon.sh
@@ -8,7 +8,7 @@ cd $PBS_O_WORKDIR
 #Description: Somatic Amplicon Pipeline (Illumina paired-end). Not for use with other library preps/ experimental conditions.
 #Author: Matt Lyon, All Wales Medical Genetics Lab
 #Mode: BY_SAMPLE
-version="1.7.5"
+version="1.7.6"
 
 # Directory structure required for pipeline
 #
